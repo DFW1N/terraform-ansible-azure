04e224a2828e3adb195b7894774c65959ed47d39
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-06-20 09:17:25 +1000

    Update ansible-autosetup.sh

--- ansible-autosetup.sh
+++ ansible-autosetup.sh
@@ -7,5 +7,4 @@ apt-add-repository ppa:ansible/ansible | echo -ne '\n';
 apt-get update;
 apt-get install ansible -y;
 ansible --version;
-(sleep 3; echo) | useradd -m -p ansible -s /bin/bash ansible;
-su ansible | (sleep 1; echo) | echo ansible;
+(sleep 1; echo) | useradd -m -s /bin/bash -p ansible ansible;
