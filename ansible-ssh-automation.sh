975a2b07fd135a0a40dcd0cedcefca0bd65e7c0f
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-06-20 09:56:34 +1000

    Update ansible-ssh-automation.sh

--- ansible-ssh-automation.sh
+++ ansible-ssh-automation.sh
@@ -2,12 +2,12 @@
 
 mkdir ~/.azure && cd ~/.azure;
 curl https://raw.githubusercontent.com/DFW1N/ansible-tower/main/credentials -o credentials;
-echo "Please type input values for your SSH or just press enter for default.";
+echo "\e[95mPlease type input values for your SSH or just press enter for default.\e[49m";
 ssh-keygen -t rsa;
 chmod 755 ~/.ssh;
 touch ~/.ssh/authorized_keys;
 chmod 644 ~/.ssh/authorized_keys;
-echo "Please type input value of YES to continue for the authenticity of host 127.0.0.1";
+echo "\e[95mPlease type input value of YES to continue for the authenticity of host 127.0.0.1\e[49m";
 ssh-copy-id -i adminuser@127.0.0.1;
 cat ~/.ssh/id_rsa;
 echo "------------------------------------------------------------IMPORTANT------------------------------------------------------------------------";
