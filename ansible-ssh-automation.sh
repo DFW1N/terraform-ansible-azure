7448cedb05423797b214cfd8a5090ef3f05ea047
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-06-20 09:50:40 +1000

    Update ansible-ssh-automation.sh

--- ansible-ssh-automation.sh
+++ ansible-ssh-automation.sh
@@ -2,8 +2,9 @@
 
 mkdir ~/.azure && cd ~/.azure;
 curl https://raw.githubusercontent.com/DFW1N/ansible-tower/main/credentials -o credentials;
-ssh-keygen -t rsa | echo -ne '\n' | echo -ne '\n';
+ssh-keygen -t rsa;
 chmod 755 ~/.ssh;
 touch ~/.ssh/authorized_keys;
 chmod 644 ~/.ssh/authorized_keys;
 ssh-copy-id -i adminuser@127.0.0.1;
+cat ~/.ssh/id_rsa;
