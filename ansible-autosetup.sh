4489467345b723da7b80791edabe01c2b744dd6e
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-06-20 08:26:41 +1000

    Update ansible-autosetup.sh

--- ansible-autosetup.sh
+++ ansible-autosetup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-sudo apt update;sudo apt upgrade -y;apt-get install apt-transport-https wget gnupg -y;apt-add-repository ppa:ansible/ansible
+mkdir ~/.azure && cd ~/.azure;
+curl https://raw.githubusercontent.com/DFW1N/ansible-tower/main/credentials -o credentials;
+ssh-keygen -t rsa;
+chmod 755 ~/.ssh;
+touch ~/.ssh/authorized_keys;
+chmod 644 ~/.ssh/authorized_keys;
+ssh-copy-id adminuser@127.0.0.1;
+sudo apt update;
+sudo apt upgrade -y;
+apt-get install apt-transport-https wget gnupg;
+apt-add-repository ppa:ansible/ansible
+apt-get update;
+apt-get install ansible -y;
+ansible --version
