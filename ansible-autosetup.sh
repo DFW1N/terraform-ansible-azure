d4085f5a491cf212db873289fa16bcb6d521a75c
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-06-20 09:38:59 +1000

    Update ansible-autosetup.sh

--- ansible-autosetup.sh
+++ ansible-autosetup.sh
@@ -8,5 +8,5 @@ apt-get update;
 apt-get install ansible -y;
 ansible --version;
 (sleep 1; echo) | useradd -m -s /bin/bash ansible;
-echo Your Ansible version has displayed above confirming successful installation also an ansible account has been created.;
-echo Please use "sudo passwd ansible" to change the user account password and complete setup with "ssh-keygen".;
+echo -e "\e[95m Your Ansible version has displayed above confirming successful installation also an ansible account has been created.";
+echo -e "Please use \e[101m sudo passwd ansible \e[49m to create the user account password and complete setup with \e[101m ssh-keygen \e[49m command.";
