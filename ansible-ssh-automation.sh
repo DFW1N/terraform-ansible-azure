860bee81d68f137167b465340ef98d03383d9e7b
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-06-20 08:41:14 +1000

    Create ansible-ssh-automation.sh

---
+++ ansible-ssh-automation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir ~/.azure && cd ~/.azure;
+curl https://raw.githubusercontent.com/DFW1N/ansible-tower/main/credentials -o credentials;
+ssh-keygen -t rsa;
+chmod 755 ~/.ssh;
+touch ~/.ssh/authorized_keys;
+chmod 644 ~/.ssh/authorized_keys;
+ssh-copy-id -i adminuser@127.0.0.1;
