a2f9769e8de7ae71e72af180ed553230ae1f52c7
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-06-20 10:02:57 +1000

    Update ansible-ssh-automation.sh

--- ansible-ssh-automation.sh
+++ ansible-ssh-automation.sh
@@ -2,14 +2,15 @@
 
 mkdir ~/.azure && cd ~/.azure;
 curl https://raw.githubusercontent.com/DFW1N/ansible-tower/main/credentials -o credentials;
-echo -e "\e[95mPlease type input values for your SSH or just press enter for default.\n";
+echo "Please type input values for your SSH or just press enter for default.";
 ssh-keygen -t rsa;
 chmod 755 ~/.ssh;
 touch ~/.ssh/authorized_keys;
 chmod 644 ~/.ssh/authorized_keys;
-echo -e "\e[95mPlease type input value of YES to continue for the authenticity of host 127.0.0.1\n";
+echo "Please type input value of YES to continue for the authenticity of host 127.0.0.1";
 ssh-copy-id -i adminuser@127.0.0.1;
 cat ~/.ssh/id_rsa;
 echo "------------------------------------------------------------IMPORTANT------------------------------------------------------------------------";
 echo "Please view directory ~/.azure/credentials for file to input Azure Service Principal values";
+echo "Please copy the RSA above into your Azure DevOps environment to create SSH Service Connection";
 echo "------------------------------------------------------------IMPORTANT------------------------------------------------------------------------";
