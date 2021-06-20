a7ce9f1c01cf812d9aa0695cf142cc457285e599
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-06-20 14:01:39 +1000

    Update ansible-autosetup.sh

--- ansible-autosetup.sh
+++ ansible-autosetup.sh
@@ -10,6 +10,14 @@ ansible --version;
 (sleep 1; echo) | useradd -m -s /bin/bash ansible;
 echo "------------------------------------------------------------IMPORTANT------------------------------------------------------------------------";
 echo "\e[95m Your Ansible version has displayed above confirming successful installation also an ansible account has been created.";
-echo "Please use \e[101m sudo passwd ansible \e[49m to create the user account password and complete setup with \e[101m ssh-keygen \e[49m command.";
+echo "Please use sudo passwd ansible to create the user account password and complete setup with ssh-keygen command.";
 echo "---------------------------------------------------------------------------------------------------------------------------------------------";
+sudo apt install python3-pip -y;
+sudo apt install python-pip -y;
+wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt;
+echo "---------------------------------------------------Checking pip3 installation version-------------------------------------------------------";
+pip3 --version;
+sudo pip3 install -r requirements-azure.txt;
+pip3 install ansible[azure];
 sudo chmod +x ansible-ssh-automation.sh;
+
