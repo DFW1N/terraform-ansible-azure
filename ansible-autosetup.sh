ba41a90a1d16fb92ca0aa0ac1f87e326d9b09cda
DFW1N <45083490+DFW1N@users.noreply.github.com>
2021-06-22 21:52:57 +1000

    Update ansible-autosetup.sh

--- ansible-autosetup.sh
+++ ansible-autosetup.sh
@@ -2,21 +2,21 @@
 
 sudo apt update;
 sudo apt upgrade -y;
-apt-get install apt-transport-https wget gnupg -y;
-apt-add-repository ppa:ansible/ansible | echo -ne '\n';
-apt-get update;
-apt-get install ansible -y;
+sudo apt-add-repository ppa:ansible/ansible | echo -ne '\n';
+sudo apt-get update;
+sudo apt install ansible -y;
 ansible --version;
-(sleep 1; echo) | useradd -m -s /bin/bash ansible;
-echo "------------------------------------------------------------IMPORTANT------------------------------------------------------------------------";
-echo "Your Ansible version has displayed above confirming successful installation also an ansible account has been created.";
-echo "Please use sudo passwd ansible to create the user account password and complete setup with ssh-keygen command.";
-echo "---------------------------------------------------------------------------------------------------------------------------------------------";
-curl -fsSL -o- https://bootstrap.pypa.io/pip/3.5/get-pip.py | python3.5;
-wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt;
-echo "---------------------------------------------------Checking pip3 installation version-------------------------------------------------------";
-pip3 --version;
-sudo pip3 install -r requirements-azure.txt;
-pip3 install ansible[azure];
+
+#(sleep 1; echo) | useradd -m -s /bin/bash ansible;
+#echo "------------------------------------------------------------IMPORTANT------------------------------------------------------------------------";
+#echo "Your Ansible version has displayed above confirming successful installation also an ansible account has been created.";
+#echo "Please use sudo passwd ansible to create the user account password and complete setup with ssh-keygen command.";
+#echo "---------------------------------------------------------------------------------------------------------------------------------------------";
+#curl -fsSL -o- https://bootstrap.pypa.io/pip/3.5/get-pip.py | python3.5;
+#wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt;
+#echo "---------------------------------------------------Checking pip3 installation version-------------------------------------------------------";
+#pip3 --version;
+#sudo pip3 install -r requirements-azure.txt;
+#pip3 install ansible[azure];
 sudo chmod +x ansible-ssh-automation.sh;
 
